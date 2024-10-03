fe11901b42d57d1960f3eb35680df3095c413005
Niccolo <niccolomancini3@gmail.com>
2024-10-03 09:30:11 +0200

    fine esercizio link

---
+++ esercizio11-Informatica.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+        <?php
+            $linkBase = "https://xkcd.com";
+            $numLink = rand(2, 5);
+            for ($i=1; $i <= $numLink; $i++) { 
+                echo "<a href='{$linkBase}/{$i}' target='_blank'> Pagina $i </a><br>";
+            }
+        ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
